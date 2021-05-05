68c35c59cbaea739fc810f145fb6d8b3047382fb
Valter Monteiro <valtinms@gmail.com>
2021-05-05 17:56:06 -0400

    Basic static components

---
+++ frontend/src/Components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/ValterMonteiro" target="_blank" rel="noreferrer">Valter Monteiro</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+      Evento promovido pela escola DevSuperior: <a href="https://vmscon.com" target="_blank" rel="noreferrer">VMS CONTABILIDADE</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
