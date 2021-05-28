73a2aebca768cd7e6ed863d339abb4ba4e17b82d
Valter Monteiro <valtinms@gmail.com>
2021-05-28 17:11:40 -0400

    Pagination

--- frontend/src/Components/Footer/index.tsx
+++ frontend/src/Components/Footer/index.tsx
@@ -4,7 +4,7 @@ const Footer = () => {
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/ValterMonteiro" target="_blank" rel="noreferrer">Valter Monteiro</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-      Evento promovido pela escola DevSuperior: <a href="https://vmscon.com" target="_blank" rel="noreferrer">VMS CONTABILIDADE</a></small></p>
+      Evento promovido pela escola DevSuperior: <a href="https://vmscon.com" target="_blank" rel="noreferrer">VMS CONTABILIDADE </a></small></p>
             </div>
         </footer>
     );
--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,12 +1,13 @@
 import { Seller } from "./seller"
 
+
 export type Sale = {
     id: number;
     visited: number;
     deals: number;
     amount: number;
     date: string;
-    seller: Seller;
+    seller: Seller
 }
 
 export type SalePage = {
